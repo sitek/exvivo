28af67364af1fb55d68f9ba3f6848c26079cb40a
sitek <sitek.kevin@gmail.com>
2017-07-27 11:51:01 -0400

    Fixed refmask flag

--- maastricht/fnirt_maas_to_exvivo.sh
+++ maastricht/fnirt_maas_to_exvivo.sh
@@ -7,8 +7,9 @@
 fnirt --ref=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz \
       --in=/om/user/ksitek/maastricht/brainstem/fnirt_anat_siT1w_fslreorient2std.2.nii.gz \
       --aff=/om/user/ksitek/exvivo/maastricht/maas7t_flirt_exvivo-space.mat \
-      --applyrefmask=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_mask.nii.gz \
-      --warpres=4,4,4 \
-      --cout=om/user/ksitek/exvivo/maastricht/maas2exvivo_warp4_coef
-      --iout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warp4.nii.gz \
-      --fout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warp4_warpfield
+      --refmask=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_mask.nii.gz \
+      --warpres=2,2,2 \
+      --interp=spline \
+      --cout=om/user/ksitek/exvivo/maastricht/maas2exvivo_warp2-spline_coef \
+      --iout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warp2-spline.nii.gz \
+      --fout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warpfield_warp2-spline
--- maastricht/fnirt_maas_to_exvivo_faruk.sh
+++ maastricht/fnirt_maas_to_exvivo_faruk.sh
@@ -7,7 +7,7 @@
 fnirt --ref=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz \
       --in=/om/user/ksitek/maastricht/brainstem/fnirt_anat_siT1w_fslreorient2std.2.nii.gz \
       --aff=/om/user/ksitek/exvivo/maastricht/maas7t_flirt_exvivo-space.mat \
-      --applyrefmask=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_mask.nii.gz \
+      --refmask=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_mask.nii.gz \
       --subsamp=2,2,1,1 \
       --miter=100,100,50,50 \
       --infwhm=2,2,1,1 \
