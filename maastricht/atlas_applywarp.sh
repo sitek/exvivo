84822792762c6813a6a86855b91801d0519229ae
sitek <sitek.kevin@gmail.com>
2017-08-23 14:33:13 -0400

    simplified sbatch args; changed ref to higher res target

--- maastricht/atlas_applywarp.sh
+++ maastricht/atlas_applywarp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --mem=250G
-#SBATCH --time=16:00:00
+#SBATCH --mem=50G
+#SBATCH --time=1:00:00
 #SBATCH --qos=gablab
 
 #fslreorient2std /om/user/ksitek/maastricht/brainstem/corr_group_bin_thr0pt2_c300_bin_labels.nii.gz \
 #                /om/user/ksitek/maastricht/brainstem/corr_group_bin_thr0pt2_c300_bin_labels_fslreorient2std.nii.gz
 
-applywarp --ref=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_faruk-param.nii.gz \
+applywarp --ref=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice.nii.gz \
       --in=/om/user/ksitek/maastricht/brainstem/corr_group_bin_thr0pt2_c300_bin_labels_fslreorient2std_200um.nii.gz \
-      --out=/om/user/ksitek/exvivo/maastricht/maas-atlas2exvivo_faruk-param.nii.gz \
+      --out=/om/user/ksitek/exvivo/maastricht/maas-atlas2exvivo_warp2-spline.nii.gz \
       --premat=/om/user/ksitek/exvivo/maastricht/maas7t_flirt_exvivo-space.mat \
-      --warp=/om/user/ksitek/exvivo/maastricht/maas2exvivo_faruk-param.nii.gz \
+      --warp=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warpfield_warp2-spline.nii.gz \
       --interp=spline -v
