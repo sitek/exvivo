82d682b25d89c8404b955a071b2d4090b55a6a74
sitek <sitek.kevin@gmail.com>
2017-08-23 16:35:41 -0400

    added variables, flag values

--- maastricht/fnirt_maas_to_exvivo.sh
+++ maastricht/fnirt_maas_to_exvivo.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 #SBATCH --mem=250G
-#SBATCH --time=16:00:00
+#SBATCH --time=15:00:00
 #SBATCH --qos=gablab
+
+maas_dir='/om/user/ksitek/exvivo/maastricht/'
+
+warp=1
+interp='spline'
 
 fnirt --ref=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz \
       --in=/om/user/ksitek/maastricht/brainstem/fnirt_anat_siT1w_fslreorient2std.2.nii.gz \
-      --aff=/om/user/ksitek/exvivo/maastricht/maas7t_flirt_exvivo-space.mat \
+      --aff=${maas_dir}maas7t_flirt_exvivo-space.mat \
       --refmask=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_mask.nii.gz \
-      --warpres=2,2,2 \
-      --interp=spline \
-      --cout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_warp2-spline_coef \
-      --iout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warp2-spline.nii.gz \
-      --fout=/om/user/ksitek/exvivo/maastricht/maas2exvivo_fnirt_warpfield_warp2-spline
+      --warpres=$warp,$warp,$warp \
+      --subsamp=4,2,1,1 \
+#      --miter=100,100,50,50 \
+      --infwhm=2,2,1,1 \
+      --reffwhm=0,0,0,0 \
+      --interp=$interp \
+      --cout=${maas_dir}maas2exvivo_fnirt_warp${warp}_reffwhm0_std-miter_${interp}_cout \
+      --iout=${maas_dir}maas2exvivo_fnirt_warp${warp}_reffwhm0_std-miter_${interp}_iout \
+      --fout=${maas_dir}maas2exvivo_fnirt_warp${warp}_reffwhm0_std-miter_${interp}_fout \
+      --verbose
