36731bcf55ad03f20e916529df5a1bf34e68dee6
sitek <sitek.kevin@gmail.com>
2017-08-25 11:27:14 -0400

    script for antsRegistration, based on github wiki

--- maastricht/ants_reg.sh
+++ maastricht/ants_reg.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-# nipype documentation example's command line output:
-# http://nipype.readthedocs.io/en/latest/interfaces/generated/interfaces.ants/registration.html
+# from ants github wiki
+# https://github.com/stnava/ANTs/wiki/Anatomy-of-an-antsRegistration-call
 
 #SBATCH --qos=gablab
 #SBATCH --time=20:00:00
 #SBATCH --mem=250G
 
-mat=/om/user/ksitek/exvivo/maastricht/maas7t_flirt_exvivo-space.mat
 fixed_img=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz
 moving_img=/om/user/ksitek/maastricht/brainstem/fnirt_anat_siT1w_fslreorient2std.2.nii.gz
-out_img=/om/user/ksitek/exvivo/maastricht/maas2exvivo_ants_warped_image.nii.gz
+out_template=/om/user/ksitek/exvivo/maastricht/maas2exvivo_ants_
 
-antsRegistration --collapse-output-transforms 0 --dimensionality 3 \
-  --initial-moving-transform [ $mat, 0 ] --initialize-transforms-per-stage 0 \
-  --interpolation Linear --output [ output_, out_img ]  --transform Affine[ 2.0 ] \
-  --metric Mattes[ fixed_img, moving_img, 1, 32, Random, 0.05 ] \
-  --convergence [ 1500x200, 1e-08, 20 ] --smoothing-sigmas 1.0x0.0vox --shrink-factors 2x1 \
-  --use-estimate-learning-rate-once 1 --use-histogram-matching 1 \
-  --transform SyN[ 0.25, 3.0, 0.0 ] \
-  --metric Mattes[ fixed_img, moving_img, 1, 32 ] \
-  --convergence [ 100x50x30, 1e-09, 20 ] --smoothing-sigmas 2.0x1.0x0.0vox --shrink-factors 3x2x1 \
-  --use-estimate-learning-rate-once 1 --use-histogram-matching 1 \
-  --winsorize-image-intensities [ 0.0, 1.0 ]  --write-composite-transform 1
+antsRegistration --dimensionality 3 --float 0 \
+        --output [$out_template, ${out_template}Warped.nii.gz] \
+        --interpolation Linear \
+        --winsorize-image-intensities [0.005,0.995] \
+        --use-histogram-matching 0 \
+        --initial-moving-transform [$fixed_img,$moving_img,1] \
+        --transform Rigid[0.1] \
+        --metric MI[$fixed_img,$moving_img,1,32,Regular,0.25] \
+        --convergence [1000x500x250x100,1e-6,10] \
+        --shrink-factors 8x4x2x1 \
+        --smoothing-sigmas 3x2x1x0vox \
+        --transform Affine[0.1] \
+        --metric MI[$fixed_img,$moving_img,1,32,Regular,0.25] \
+        --convergence [1000x500x250x100,1e-6,10] \
+        --shrink-factors 8x4x2x1 \
+        --smoothing-sigmas 3x2x1x0vox \
+        --transform SyN[0.1,3,0] \
+        --metric CC[$fixed_img,$moving_img,1,4] \
+        --convergence [100x70x50x20,1e-6,10] \
+        --shrink-factors 8x4x2x1 \
+        --smoothing-sigmas 3x2x1x0vox
