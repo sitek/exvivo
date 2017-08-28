19ddec1919cbe3e779641113450db3b4b8ec46b0
sitek <sitek.kevin@gmail.com>
2017-08-28 14:51:20 -0400

    updated for ants registration

--- maastricht/extract_cluster.sh
+++ maastricht/extract_cluster.sh
@@ -3,8 +3,10 @@
 #SBATCH --qos=gablab
 #SBATCH --time=1:00:00
 
+transformed_atlas=/om/user/ksitek/exvivo/maastricht/atlas/maas-atlas2exvivo_ants.nii.gz
+
 for i in 1 2 3 4 5 6 7 8; do
-    fslmaths /om/user/ksitek/exvivo/maastricht/maas-atlas2exvivo_fnirtref_warpcoef.nii.gz \
+    fslmaths ${transformed_atlas} \
              -thr $i -uthr $i -bin \
-             /om/user/ksitek/exvivo/maastricht/maas-atlas2exvivo_cluster-${i};
+             /om/user/ksitek/exvivo/maastricht/roi_ants/maas-atlas2exvivo_cluster-${i};
     done
