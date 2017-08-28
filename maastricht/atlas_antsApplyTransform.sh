182d7de1c85942a143a32c630e3459e05cd234b4
sitek <sitek.kevin@gmail.com>
2017-08-28 14:48:35 -0400

    apply ants transform to atlas

---
+++ maastricht/atlas_antsApplyTransform.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --mem=50G
+#SBATCH --time=1:00:00
+#SBATCH --qos=gablab
+
+data_dir=/om/user/ksitek/exvivo/data/
+maas_dir=/om/user/ksitek/exvivo/maastricht/
+maas_in_dir=/om/user/ksitek/maastricht/brainstem/
+
+antsApplyTransforms \
+  --reference-image ${data_dir}Reg_S64550_nii_b0-slice.nii.gz \
+  --input ${maas_in_dir}corr_group_bin_thr0pt2_c300_bin_labels_fslreorient2std_200um.nii.gz \
+  --output ${maas_dir}atlas/maas-atlas2exvivo_ants.nii.gz \
+  --transform ${maas_dir}ants/maas2exvivo_ants_1Warp.nii.gz \
+  --transform ${maas_dir}ants/maas2exvivo_ants_0GenericAffine.mat \
+  --interpolation BSpline
