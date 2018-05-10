98be017fbd744a65f858db1230dd45d4efb32869
sitek <sitek.kevin@gmail.com>
2018-05-10 13:51:44 -0400

    apply transform to ex vivo space

--- maastricht/atlas_antsApplyTransform.sh
+++ maastricht/atlas_antsApplyTransform.sh
@@ -8,10 +8,12 @@ data_dir=/om/user/ksitek/exvivo/data/
 maas_dir=/om/user/ksitek/exvivo/maastricht/
 maas_in_dir=/om/user/ksitek/maastricht/brainstem/
 
+interp=Linear
+
 antsApplyTransforms \
   --reference-image ${data_dir}Reg_S64550_nii_b0-slice.nii.gz \
   --input ${maas_in_dir}corr_group_bin_thr0pt2_c300_bin_labels_fslreorient2std_200um.nii.gz \
-  --output ${maas_dir}atlas/maas-atlas2exvivo_ants.nii.gz \
+  --output ${maas_dir}atlas/maas-atlas2exvivo_ants_${interp}.nii.gz \
   --transform ${maas_dir}ants/maas2exvivo_ants_1Warp.nii.gz \
   --transform ${maas_dir}ants/maas2exvivo_ants_0GenericAffine.mat \
-  --interpolation BSpline
+  --interpolation ${interp}
