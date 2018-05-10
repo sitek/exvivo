6e7415238884bb3807d874dad95f5c1bd30731b0
sitek <sitek.kevin@gmail.com>
2018-05-10 13:50:03 -0400

    added registration with in vivo brainstem

--- hi-res/ants_reg.sh
+++ hi-res/ants_reg.sh
@@ -4,12 +4,16 @@
 # https://github.com/stnava/ANTs/wiki/Anatomy-of-an-antsRegistration-call
 
 #SBATCH --qos=gablab
-#SBATCH --time=20:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem=250G
 
-fixed_img=/om/user/ksitek/exvivo/data/S64520_m0.nii.gz
-moving_img=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz
-out_template=/om/user/ksitek/exvivo/data/Reg_S64550_to_S64520_ants
+fixed_base=S64520_m0_200um
+fixed_img=/om2/user/ksitek/exvivo/data/${fixed_base}.nii.gz
+
+#moving_img=/om/user/ksitek/exvivo/data/Reg_S64550_nii_b0-slice_fslreorient2std.nii.gz
+#out_template=/om2/user/ksitek/exvivo/data/Reg_S64550_to_S64520_ants
+moving_img=/om2/user/ksitek/hcp_7t/data/100610/T1w/T1w_acpc_dc_bs-dc-thal.nii.gz
+out_template=/om2/user/ksitek/exvivo/data/${fixed_base}_to_hcp100610_T1w
 
 antsRegistration --dimensionality 3 --float 0 \
         --output [$out_template, ${out_template}Warped.nii.gz] \
