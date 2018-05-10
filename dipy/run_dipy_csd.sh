aeb0485d0fbb1634c0930c1280e7281baef135fb
sitek <sitek.kevin@gmail.com>
2018-05-10 13:49:23 -0400

    recent changes

--- dipy/run_dipy_csd.sh
+++ dipy/run_dipy_csd.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-python dipy_csd.py
+#SBATCH --time=4:00:00
+#SBATCH --mem=100GB
+#SBATCH --cpus-per-task=10
+
+script_dir=/om2/user/ksitek/exvivo/scripts/dipy/
+python ${script_dir}dipy_csd.py
